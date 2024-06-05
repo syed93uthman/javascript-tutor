6677d9ac8e62f87c06d651c6d943b9b639cd3f34
syed93uthman <syed_uthman@yahoo.com>
2024-06-06 07:13:50 +0800

    function with retur value

--- Note1-Function.js
+++ Note1-Function.js
@@ -1,6 +1,16 @@
+//Function with Parameters
 function greet(firstName,lastName) {
     console.log("Hello "+firstName+" "+lastName);
 }
 
 greet("ali","ahmad");
-greet("abu","ahmad");
+
+//Function with Return Value
+let result = 0;
+
+function addition(value1,value2){
+    return value1 + value2;
+}
+
+result = addition(5,6);
+console.log(result);
